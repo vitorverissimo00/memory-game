435484ee0ddef13c92277d4f0c438e2b9e08983f
Vítor Azevedo <vigsoft410@gmail.com>
2025-02-11 02:30:30 -0300

    :wrench: Add initial project setup with TypeScript, styled-components, and basic GameBoard component :wrench:

---
+++ src/containers/GameBoard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainContainer } from './styles'
+
+const GameBoard = () => {
+  return (
+    <MainContainer>
+      <span>GameBoard</span>
+    </MainContainer>
+  )
+}
+
+export default GameBoard
